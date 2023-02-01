823f16ab9de2b5a22985b30666f9b277eeda53fa
cheon4050 <ehdcjs159@gmail.com>
2023-02-02 03:13:40 +0900

    1.5.1: 오브젝트 팩토리를 이용한 스프링 IOC

--- src/domain/DaoFactory.java
+++ src/domain/DaoFactory.java
@@ -1,11 +1,16 @@
 package domain;
 
-public class DaoFactory {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new SimpleConnectionMaker();
     }
